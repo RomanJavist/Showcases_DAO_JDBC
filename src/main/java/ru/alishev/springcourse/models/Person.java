569ababda0f728e6e2b06248f4400f57f8fd2dba
Roman Javist <roman.javist@gmail.com>
2023-08-10 21:02:03 +0700

    Initial commit

---
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -0,0 +1,50 @@
+package ru.alishev.springcourse.models;
+
+import javax.validation.constraints.*;
+
+/**
+ * @author Neil Alishev
+ */
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть длиной от 2 до 100 символов")
+    private String fullName;
+
+    @Min(value = 1925, message = "Год рождения должен быть больше, чем 1925")
+    @Max(value = 2005, message = "Год рождения должен быть меньше, чем 2005")
+    private int yearOfBirth;
+
+    public Person() {
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
